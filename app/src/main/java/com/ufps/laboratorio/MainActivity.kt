428448b207681eb478f82947ddd4a2f5c1c98475
Camila Calderon <angelycamilacg@ufps.edu.co>
2022-06-04 20:21:58 -0500

    Inicio sesion con google

--- app/src/main/java/com/ufps/laboratorio/MainActivity.kt
+++ app/src/main/java/com/ufps/laboratorio/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.ufps.laboratorio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+    var gso: GoogleSignInOptions? = null
+    var gsc: GoogleSignInClient? = null
+    var googlebtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        googlebtn = findViewById(R.id.googleButton)
+        gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        gsc = GoogleSignIn.getClient(this, gso!!)
+       // googlebtn.setOnClickListener(View.OnClickListener { signIn() })
+    }
+
+    fun signIn() {
+        val signInIntent = gsc!!.signInIntent
+        startActivityForResult(signInIntent, 1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            navigateToSecondActivity() //Si inicia sesión, se dirige a la otra pantalla
+            try {
+                task.getResult(ApiException::class.java)
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Algo no está sirviendo", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    fun navigateToSecondActivity() {
+        finish()
+        //val intent = Intent(this@MainActivity, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
